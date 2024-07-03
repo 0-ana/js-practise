1fea7062e699219a3f916a59b6b094286e5681ed
Ana Ivanovic <ana.ivanovic0901@gmail.com>
2024-07-03 19:03:59 +0000

    Add all questions

---
+++ assets/js/script.js
@@ -0,0 +1,162 @@
+let allQuestions [
+    {
+        question: 'How long is an Olympic swimming pool (in meters)?'
+        option1: '40'
+        option2: '45'
+        option3: '50'
+        option4: '55'
+        answer: '50'
+    };
+    {
+        question: 'What is "cynophobia"?'
+        option1: 'Fear of dogs'
+        option2: 'Fear of cats'
+        option3: 'Fear of heights'
+        option4: 'Fear of crowds'
+        answer: 'Fear of dogs'
+    }
+    {
+        question: 'What is the name of the World largest ocean?'
+        option1: 'Atlantic Ocean'
+        option2: 'Indian Ocean'
+        option3: 'Pacific Ocean'
+        option4: 'Arctic Ocean'
+        answer: 'Pacific Ocean'
+    }
+    {
+        question: 'How many colors are there in the rainbow?'
+        option1: 'Five'
+        option2: 'Six'
+        option3: 'Seven'
+        option4: 'Eight'
+        answer: 'Seven'
+    }
+    {
+        question: 'Which country is the smallest in the world?'
+        option1: 'Tuvalu'
+        option2: 'Nauru'
+        option3: 'Monaco'
+        option4: 'Vatican City'
+        answer: 'Vatican City'
+    }
+    {
+        question: 'Area 51 is located in which U S state?'
+        option1: 'Oregon'
+        option2: 'Idaho'
+        option3: 'Utah'
+        option4: 'Nevada'
+        answer: 'Nevada'
+    }
+    {
+        question: 'What country touches the Indian Ocean, the Arabian Sea, and the Bay of Bengal?'
+        option1: 'Bangladesh'
+        option2: 'India'
+        option3: 'Sri Lanka'
+        option4: 'Maldives'
+        answer: 'India'
+    }
+    {
+        question: 'What country has the most natural lakes?'
+        option1: 'Canada'
+        option2: 'Finland'
+        option3: 'Switzerland'
+        option4: 'Russia'
+        answer: 'Canada'
+    }
+    {
+        question: 'How many hearts does an octopus have?'
+        option1: 'Two'
+        option2: 'Three'
+        option3: 'Four'
+        option4: 'Five'
+        answer: 'Three'
+    }
+    {
+        question: 'How many legs does a spider have?'
+        option1: 'Four'
+        option2: 'Six'
+        option3: 'Eight'
+        option4: 'Ten'
+        answer: 'Eight'
+    }
+    {
+        question: 'What is the largest planet in the solar system?'
+        option1: 'Saturn'
+        option2: 'Venus'
+        option3: 'Jupiter'
+        option4: 'Neptune'
+        answer: 'Jupiter'
+    }
+    {
+        question: 'What is the fastest land animal?'
+        option1: 'Quarter Horse'
+        option2: 'Pronghorn'
+        option3: 'Springbok'
+        option4: 'Cheetah'
+        answer: 'Cheetah'
+    }
+    {
+        question: 'Who painted the Mona Lisa?'
+        option1: 'Vincent van Gogh'
+        option2: 'Claude Monet'
+        option3: 'Leonardo da Vinci'
+        option4: 'Pablo Picasso'
+        answer: 'Leonardo da Vinci'
+    }
+    {
+        question: 'What nut is in the middle of a Ferrero Rocher?'
+        option1: 'Hazelnut'
+        option2: 'Almond'
+        option3: 'Walnut'
+        option4: 'Peanut'
+        answer: 'Hazelnut'
+    }
+    {
+        question: 'How many rings does olympics logo have?'
+        option1: 'Four'
+        option2: 'Five'
+        option3: 'Six'
+        option4: 'Seven'
+        answer: 'Five'
+    }
+    {
+        question: 'How many elements are there in the periodic table?'
+        option1: '104'
+        option2: '108'
+        option3: '114'
+        option4: '118'
+        answer: '118'
+    }
+    {
+        question: 'Zagreb is the capital city of which country?'
+        option1: 'Croatia'
+        option2: 'Bosnia and Herzegovina'
+        option3: 'Serbia'
+        option4: 'Slovenia'
+        answer: 'Croatia'
+    }
+    {
+        question: 'What year did the Berlin Wall fall?'
+        option1: '1986'
+        option2: '1987'
+        option3: '1988'
+        option4: '1989'
+        answer: '1989'
+    }
+    {
+        question: 'What is the largest organ in the human body?'
+        option1: 'Skin'
+        option2: 'Lungs'
+        option3: 'liver'
+        option4: 'Heart'
+        answer: 'Skin'
+    }
+    {
+        question: 'In what year did the Titanic sink?'
+        option1: '1912'
+        option2: '1913'
+        option3: '1914'
+        option4: '1915'
+        answer: '1912'
+    }
+]
